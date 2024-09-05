e53b6ac676766b88af337e72bf922836a9bd0e6a
Ben Cotton <ben@kusari.dev>
2024-09-05 12:19:53 -0400

    Make the GitHub token file optional
    You probably want to use it, but maybe you're feeling lucky.

--- guactober.py
+++ guactober.py
@@ -1,13 +1,37 @@
 import json
 import re
+import os.path
 from github import Github
 
-with open('.github_token') as gh_token_file:
-    github_token = gh_token_file.read().strip()
-    gh_token_file.close()
+###
+#
+# Things you might need to change
+#
+###
+
+# File containing a PAT or classic token for GitHub authentication
+# If this file does not exist, we'll use an unauthenticated session,
+# which probably means you'll get rate limited.
+GITHUB_TOKEN_FILE='.github_token'
+
+###
+#
+# Things you probably won't need to change
+#
+###
+
+# Test for a GitHub token file and setup the GitHub session
+if os.path.exists(GITHUB_TOKEN_FILE):
+    with open('.github_token') as gh_token_file:
+        github_token = gh_token_file.read().strip()
+        gh_token_file.close()
+        github_session = Github(github_token)
+else:
+    github_session = Github()
+    print("Using unauthenticated session for GitHub," + \
+          "you may get rate limited!")
 
 print("Getting list of Hacktoberfest repos from GitHub (be patient!)")
-github_session = Github(github_token)
 hacktoberfest_deps = []
 hacktoberfest_repos = []
 response = github_session.search_repositories(query=f'topic:hacktoberfest')
